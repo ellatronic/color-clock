de2eb440e8e979de3245501a1d9c48660413bd76
ellatronic <ellachiangg@gmail.com>
2015-10-20 20:37:20 -0700

    fixed issue: no color generation with single digits

--- script.js
+++ script.js
@@ -5,9 +5,34 @@ $(document).ready(function(){
 
   // 1. Get the current time every second (24 hour time)
     function changeColor() {
-      var now = new Date(Date.now());
-      var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-      var color = "#" + now.getHours() + now.getMinutes() + now.getSeconds();
+      var now = new Date();
+      var hours;
+      var minutes;
+      var seconds;
+
+      if (parseInt(now.getHours()) < 10) {
+        hours = "0" + now.getHours();
+      }
+      else {
+        hours = now.getHours();
+      }
+
+      if (parseInt(now.getMinutes()) < 10) {
+        minutes = "0" + now.getMinutes();
+      }
+      else {
+        minutes = now.getMinutes();
+      }
+
+      if (parseInt(now.getSeconds()) < 10) {
+        seconds = "0" + now.getSeconds();
+      }
+      else {
+        seconds = now.getSeconds();
+      }
+
+      var formatted = hours + ":" + minutes + ":" + seconds;
+      var color = "#" + hours + minutes + seconds;
 
   // 2. Change the background color of the page to that color
       $("body").css("background-color", color);
